c1c4cfb9aaecd015df8ea50995bf26e4fc8d6f3f
GabreSF <gabriellopesschifelbein@gmail.com>
2023-05-01 23:47:23 -0300

    Validator-password

--- ProjetoCadastro/AppCadastro/forms.py
+++ ProjetoCadastro/AppCadastro/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class RegisterForm(forms.ModelForm):
+    Confirme = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repita sua senha'
+        })
+    )
+
     class Meta:
         model = User
         fields = [
@@ -12,3 +20,14 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        Confirme = cleaned_data.get('Confirme')
+
+        if password != Confirme:
+            raise ValidationError({
+                'password': 'Senhas não são iguais'
+            })
